4a7f489079824f9afe0cd0f07ea4d6ead9624011
jonathan.hfaria <jonathan.hfaria@gmail.com>
2018-09-05 21:02:16 -0400

    Novo teste index.html

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DEVOPS 4LINUX.")
+        self.assertRegex(result.data.decode(), "DEVOPS 4LINUX JONATHAN.")
 
 
 if __name__ == "__main__":
